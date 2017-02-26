09a82b3f0e55887a55534fa93467fdfd327373db
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-02-26 15:07:40 +0100

    Improve

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,6 +9,8 @@ export default class Layout extends Component {
     this.searchAddr(event.target.value)
   }
 
+  // Example: 371 Route de la Côte 74290 Alex
+
   searchAddr(addr) {
     fetch(`http://api-adresse.data.gouv.fr/search/?q=${addr}`)
       .then(response => response.json())
@@ -16,11 +18,27 @@ export default class Layout extends Component {
         if (!response.features || response.features.length === 0) throw new Error('No geocode result')
         return response.features[0]
       })
-      .then(result => this.setState({ normalizedAddress: result }))
+      .then(result => {
+        this.fetchGPUInfos(result.geometry.coordinates[1], result.geometry.coordinates[0])
+        this.fetchCommuneInfos(result.properties.citycode)
+        this.setState({ normalizedAddress: result })
+      })
   }
 
-  shouldComponentUpdate(nextProps, { normalizedAddress}) {
-    return normalizedAddress !== this.state.normalizedAddress
+  fetchGPUInfos(lat, lon) {
+    fetch(`http://stmaur.cquest.org/gpu?lon=${lon}&lat=${lat}&dist=50`)
+      .then(response => response.json())
+      .then(response => {
+        if (!response.features || response.features.length === 0) return []
+        return response.features
+      })
+      .then(result => this.setState({ gpuInfos: result }))
+  }
+
+  fetchCommuneInfos(code) {
+    fetch(`https://geo.api.gouv.fr/communes/${code}?fields=nom,code,population,departement,region,codesPostaux`)
+      .then(response => response.json())
+      .then(result => this.setState({ communeInfos: result }))
   }
 
   render() {
@@ -37,7 +55,13 @@ export default class Layout extends Component {
             </div>
           </div>
         </div>
-        <UrbaViewer normalizedAddress={this.state.normalizedAddress} />
+        <div class="ui main text container">
+          <h2>Exemples</h2>
+          <ul>
+            <li>371 Route de la Côte 74290 Alex</li>
+          </ul>
+        </div>
+        <UrbaViewer normalizedAddress={this.state.normalizedAddress} gpuInfos={this.state.gpuInfos} communeInfos={this.state.communeInfos} />
       </div>
     )
   }
--- src/components/UrbaViewer.js
+++ src/components/UrbaViewer.js
@@ -1,12 +1,79 @@
 import { h, Component } from 'preact'
+import groupBy from 'lodash/groupBy'
+import { inside, point } from '@turf/turf'
+
+function Info({ geometry: { type }, properties: { libelle, typeinf } }) {
+  return <li>[{type}] {libelle} ({typeinf})</li>
+}
+
+function InfoList({ items }) {
+  return <ul>{items.map(item => <Info {...item} />)}</ul>
+}
+
+function Prescription({ geometry: { type }, properties: { libelle } }) {
+  return <li>[{type}] {libelle}</li>
+}
+
+function PrescriptionList({ items }) {
+  return <ul>{items.map(item => <Prescription {...item} />)}</ul>
+}
+
+const libelleZone = {
+  '00': 'Sans objet ou non encore définie dans le règlement',
+  '01': 'Habitat',
+  '02': 'Activité',
+  '03': 'Mixte habitat/activité',
+  '04': 'Loisirs et tourisme',
+  '05': 'Équipement',
+  '07': 'Activités agricoles',
+  '08': 'Espace naturel',
+  '09': 'Espace remarquable (littoral ou montagne)',
+  '10': 'Secteur de carrière',
+  '99': 'Autre'
+}
+
+function Zone({ destdomi, typezone }) {
+  const libelleDestination = destdomi ? libelleZone[destdomi] : 'Non renseigné'
+  return (
+    <ul>
+      <li>Type de zone : {typezone}</li>
+      <li>Destination principale : {libelleDestination}</li>
+    </ul>
+  )
+}
+
+function CommuneInfos({ nom, code, codesPostaux, population, departement, region }) {
+  return (
+    <ul>
+      <li>Nom : {nom}</li>
+      <li>Code INSEE : {code}</li>
+      <li>Codes postaux : {codesPostaux.join(', ')}</li>
+      <li>Population : {population}</li>
+      <li>Département : {departement.nom}</li>
+      <li>Région : {region.nom}</li>
+    </ul>
+  )
+}
 
 export default class UrbaViewer extends Component {
-  render({ normalizedAddress }) {
-    if (!normalizedAddress) return null
+  render({ normalizedAddress, gpuInfos, communeInfos }) {
+    if (!normalizedAddress || !gpuInfos) return null
+
+    const parsedInfos = groupBy(gpuInfos, 'properties.layer')
+    const zone = (parsedInfos.zone_urba || []).find(z => inside(normalizedAddress, z))
+
     return (
       <div class="ui main text container">
         <h1 class="ui header">Règles d'urbanisme</h1>
         <div>Analyse pour : <strong>{normalizedAddress.properties.label}</strong></div>
+        <h2>Zonage (PLU)</h2>
+        {zone ? <Zone {...zone.properties} /> : <p>Vide</p>}
+        <h2>Prescriptions</h2>
+        {parsedInfos.prescription ? <PrescriptionList items={parsedInfos.prescription} /> : <p>Aucune prescription à cette adresse</p>}
+        <h2>Informations complémentaires</h2>
+        {parsedInfos.info ? <InfoList items={parsedInfos.info} /> : <p>Aucune information recensée</p>}
+        <h2>Commune</h2>
+        <CommuneInfos {...communeInfos} />
       </div>
     )
   }
